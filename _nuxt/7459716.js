854e766d2b5f9b268ada8479016459f7f727580b
jim <96322054+jiangmingrj@users.noreply.github.com>
2022-05-12 17:56:52 +0800

    Update CNAME

---
+++ _nuxt/7459716.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{384:function(e,n,t){"use strict";t.r(n);var l={name:"AboutPage"},c=t(51),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("联系我们")])}),[],!1,null,null,null);n.default=component.exports}}]);
